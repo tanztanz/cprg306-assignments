be263d0bddfc3d0f0e54fb6b59eac0a5c0ef0c0c
tanztanz <129794928+tanztanz@users.noreply.github.com>
2024-05-27 21:06:02 -0600

    week 2 code changes

---
+++ app/week-2/student-info.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Tanzela Fatema Ali</h1>
+      <p>
+        GitHub Repository:{' '}
+        <Link href="https://github.com/tanztanz/cprg306-assignments"></Link>
+          
+        
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;

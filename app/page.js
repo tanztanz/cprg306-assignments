58482c07c97429b2e0591731fad2504b985c27cf
tanztanz <129794928+tanztanz@users.noreply.github.com>
2024-06-03 21:07:26 -0600

    update for week 3 page

--- app/page.js
+++ app/page.js
@@ -6,10 +6,12 @@ const HomePage = () => {
     <div>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        <Link href="week-2">Week 2
-        
-        </Link>
-      </p>
+        <Link href="week-2">Week 2</Link>
+      </p>  
+      <p>
+      <Link href="week-3">Week 3</Link>  
+      </p>  
+      
     </div>
   );
 };
